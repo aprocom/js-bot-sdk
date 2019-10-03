0747ef4aa1d4f47d40849f654e52cc9ed520755c
Lifreed <live-freeor-die@yandex.ru>
2019-10-03 16:56:34 +0300

    Add Peer in User class

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -6,6 +6,7 @@ import Long from 'long';
 import Record from 'dataclass';
 import { dialog } from '@dlghq/dialog-api';
 import { getOpt } from './utils';
+import Peer from './Peer';
 
 class User extends Record<User> {
   id: number = -1;
@@ -13,6 +14,7 @@ class User extends Record<User> {
   name: string = 'UNKNOWN';
   isBot: boolean = false;
   nick: null | string = null;
+  peer: null | Peer = Peer.private(-1);
 
   static from(api: dialog.User) {
     return new User({
@@ -21,6 +23,7 @@ class User extends Record<User> {
       name: api.data ? api.data.name : 'unknown',
       isBot: api.data ? getOpt(api.data.isBot, false) : false,
       nick: api.data ? getOpt(api.data.nick, null) : null,
+      peer: Peer.private(api.id),
     });
   }
 }
